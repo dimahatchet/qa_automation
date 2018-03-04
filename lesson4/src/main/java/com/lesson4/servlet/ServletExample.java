729e2268d5d03a5828e35a813798a86874059a92
dima.hatchet <dima.hatchet@gmail.com>
2018-03-04 23:22:48 +0200

    lesson4

---
+++ lesson4/src/main/java/com/lesson4/servlet/ServletExample.java
@@ -0,0 +1,26 @@
+package com.lesson4.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class ServletExample extends HttpServlet {
+
+    private MyCalcForServlet calc = new MyCalcForServlet();
+    private String expression;
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/text");
+        resp.getOutputStream().print(calc.calculateExpression(expression));
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        expression = req.getQueryString();
+    }
+}
